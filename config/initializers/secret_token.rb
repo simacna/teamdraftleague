8963528c6f4b1d1d4b08b3411c4895fbab34a70f
davidmccoy <david.mccoy@gmail.com>
2014-03-27 22:14:17 -0400

    Initialized rails and the database.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamdraftleagueApp::Application.config.secret_key_base = '5b6067188681a0b2cd1f30a46a2ab6a1547a48dc7aa50c78f592c9456462d1802b148ce34d9cc73ac6669a00346f0073e360df256c24bcce73f9114dff2d4cf5'
