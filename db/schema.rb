367c905375027be38ff3bda044556d95ab75d5bc
davidmccoy <david.mccoy@gmail.com>
2014-03-27 22:34:56 -0400

    Created teams model and table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328023018) do
+ActiveRecord::Schema.define(version: 20140328023411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20140328023018) do
     t.string  "name"
     t.integer "team_id"
     t.string  "bio"
+  end
+
+  create_table "teams", force: true do |t|
+    t.string "name"
   end
 
 end
