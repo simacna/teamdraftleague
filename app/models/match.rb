a794c77bf9f80b5018d6953f9f1fb1300bb57f6b
davidmccoy <david.mccoy@gmail.com>
2014-03-28 17:45:25 -0400

    Changed match model.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,6 @@
 class Match < ActiveRecord::Base
 	belongs_to :season
-	has_many :teams, through: :season
+	has_one :team, through: :season
+	has_one :challenger, class_name: "Team", foreign_key: "challenger_id", through: :season
 
 end
