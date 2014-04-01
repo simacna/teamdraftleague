8e4407dde49f8f60be6a71fe68fa8b591bf88c27
davidmccoy <david.mccoy@gmail.com>
2014-04-01 16:42:26 -0400

    More heroku set.

--- app/models/season.rb
+++ app/models/season.rb
@@ -24,16 +24,26 @@ class Season < ActiveRecord::Base
 		# Iterates through the array and setting each matches' column values
 		matches.each.select do |match|
 			x = Match.create
+			puts "Match created."
 			x.team_challonge_number = match["match"]["player1_id"]
+			puts "Team challonge number set."
 			x.team_id = Team.find_by(challonge_team_number: match["match"]["player1_id"]).id
+			puts "Team_id set."
 			x.challenger_challonge_number = match["match"]["player2_id"]
+			puts "Challenger challonge number set."
 			x.challenger_id = Team.find_by(challonge_team_number: match["match"]["player2_id"]).id
+			puts "Challenger_id set."
 			x.season_id = self.id
+			puts "Season_id set."
 			x.challonge_match_number = match["match"]["id"]
+			puts "Challonge match  number set."
 			x.challonge_match_csv = match["match"]["scores_csv"]
+			puts "Challonge match csv set."
 			x.challonge_week = match["match"]["round"]
+			puts "Challonge week set."
 			if match["match"]["winner_id"] != nil
 				x.winner = Team.find_by(challonge_team_number: match["match"]["winner_id"]).id
+				"Match winner set."
 			end
 			x.save
 		end
