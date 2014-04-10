4e1bccee24b3409409b8912aa142c1971bd11eab
davidmccoy <david.mccoy@gmail.com>
2014-04-09 22:48:54 -0400

    Added FactoryGirl

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  # what you want to call a user
+  factory :user do
+    name "phil"
+    email "phil@phil.com"
+  end
+
+  factory :invalid_user, class: User do
+    name nil
+    email nil
+  end
+
+  factory :admin, class: User do
+    name "admin"
+    email "admin@admin.com"
+    admin true
+  end
+
+end
